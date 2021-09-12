0e549b40f711d521d02da02ec1c37809ce02fa5f
ZUnead Ahmed <zuned1996@outlook.com>
2021-09-12 12:17:03 +0600

    updated

--- index.js
+++ index.js
@@ -1,66 +1,150 @@
 const express = require('express');
-const ObjectId = require('mongodb').ObjectID;
-const app = express();
-const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 require('dotenv').config()
 
-// MongoDB  uri credential
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7tcxm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri)
 
-// const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.7tcxm.mongodb.net:27017,cluster0-shard-00-01.7tcxm.mongodb.net:27017,cluster0-shard-00-02.7tcxm.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-11utgn-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
-
+const app = express();
 app.use(cors());
 app.use(express.json());
 // app.use(bodyParser.json());
+/************************ MongoDB  uri credential *************************/
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7tcxm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log('database connected' , uri)
+let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+/************************ 
+    Routes -- Get method 
+*************************/
+
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to News Portal Backend')
+})
+
+// Get all News
+app.get('/allnews', (req, res) => {
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(conErr => {
+    const allNewsCollection = client.db("newsPortal").collection("newsPortalAdmin");
+    allNewsCollection.find().toArray((err, documents) => {
+      err ? res.status(500).send(err) : res.send(documents)
+    })
+  })
+  client.close();
+})
+
+
+// Get all Admin
+app.get('/alladmin' ,(req, res) => {
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(conErr => {
+    console.log("all adnmin err", conErr)
+    const adminCollection = client.db("newsPortal").collection("Admin");
+    adminCollection.find().toArray((err, documents) => {
+      err ? res.status(500).send(err) : res.send(documents)
+    })
+  })
+  client.close();
+})
+
+// Get all User
+app.get('/users', (req, res) => {
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(conErr => {
+    
+    const userCollection = client.db("newsPortal").collection("user");
+    userCollection.find().toArray((err, documents) => {
+      err ? res.status(500).send(err) : res.send(documents)
+    })
+  })
+  client.close();
+})
+
+
+/************************ 
+    Routes -- Post method 
+*************************/
+app.post('/newsadd' , (req , res) => {
+  const data = req.body;
+  console.log("add new news data", data);
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(conErr => {
+    console.log("db error,", conErr)  
+    const newNewsCollection = client.db("newsPortal").collection("newsPortalAdmin");
+    newNewsCollection.insertOne(data, (err, result) => {
+      err ? res.status(500).send({message : err}) : res.send(result.ops[0].insertedCount > 0)
+      console.log("db data add error,", err) 
+    })
+  })
+  client.close();
+})
+
+
+app.post('/newadmin', (req, res) => {
+  const data = req.body;
+  console.log("add newAdmin data", data);
+  const email = req.body.email;
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(conErr => {
+    console.log("add new data arr", conErr);
+    const adminCollection = client.db("newsPortal").collection("Admin");
+    adminCollection.find({adminEmail: email}).toArray((err, admin) => {
+      if (admin.length === 0) {
+        res.send("You are already an admin", admin)
+      } else {
+        adminCollection.insertOne(data, (err, result) => {
+          err ? res.status(500).send({message: err}) : res.send(result.insertedCount > 0)
+          console.log("db data add error,", err) 
+        })        
+      }
+    })
+  })
+  client.close();
+
+})
+
+
+app.post('/loginuser', (req, res) => {
+  const loginuser = req.body;
+  console.log("login user data", loginuser);
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(conErr => {
+    const userCollection = client.db("newsPortal").collection("user");
+    userCollection.insertOne(loginuser, (err, result) => {
+      err ? res.status(500).send({message: err}) : res.send(result.insertedCount > 0)
+    })
+
+  })
+  client.close();
+
+})
+
+
+// // MongoDB database
+// client.connect(err => {
+//   const addNewsCollection = client.db("newsPortal").collection("newsPortalAdmin");
+//   const adminCollection = client.db("newsPortal").collection("Admin");
+//   const userCollection = client.db("newsPortal").collection("user");
+  
+//   console.log("db connected")
+//   console.log("db error,", err)
+
+//     app.post('/newsadd', (req, res) => {
+//       const newNews = req.body;
+//       console.log("add new img", newNews)
+//       addNewsCollection.insertOne(newNews)
+//       .then(response => {
+//         res.send(response.insertedCount > 0)
+//         console.log("data added")
+//       })
+//     })
+//   client.close();
+// });
 
 
 const port = process.env.PORT || 5000
-
-// MongoDB database
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const addNewsCollection = client.db("newsPortal").collection("newsPortalAdmin");
-  
-  console.log("db connected")
-  console.log("db error,", err)
-
-
-  // test database
-  // const data = {name : "test", price:25, quenteti: 100, city:"syl"}
-  // addNewsCollection.insertOne(data)
-  // .then(result => {
-  //   console.log("one data aded");
-  // })
-
-    app.post('/newsadd', (req, res) => {
-      const newNews = req.body;
-      console.log("add new img", newNews)
-      addNewsCollection.insertOne(newNews)
-      .then(response => {
-        res.send(response.insertedCount > 0)
-        console.log("data added")
-      })
-    })
-
-
-  // app.post('/addNews', (req, res) => {
-  //     const addNews = req.body;
-  //     console.log("data added", addNews)
-  //     addNewsCollection.insertOne(addNews)
-  //     .then(result => {
-  //         res.send(result.insertedCount > 0)
-  //     })
-  // })
-  // client.close();
-});
-
-
-app.get('/', (req, res) => {
-  res.send('Welcome to backend!')
-})
-
-app.listen(process.env.PORT || port)
+app.listen(port, err => err ? console.log("Filed to Listen on Port" , port) : console.log("Listing for Port" , port));
